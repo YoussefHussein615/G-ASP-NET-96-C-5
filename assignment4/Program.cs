73a36b9d9b5b0ab967dcb5f2236b034fdb82c389
EL3RABAWY <yshm2@sci.suezuni.edu.eg>
2026-08-01 17:31:21 +0300

    question1

--- assignment4/Program.cs
+++ assignment4/Program.cs
@@ -1,10 +1,26 @@
 ﻿namespace assignment4
 {
-    internal class Program
+
+        internal class Book
+    {
+        #region Question01_outside main 
+        private string password = "secret";
+        #endregion
+
+    }
+        internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            {
+                // Console.WriteLine(book.password);
+                //  what happened? => This line does NOT compile: 'Book.password' is inaccessible
+                //  why? => due to its protection level "private" restricts access to code
+                // inside the Book class itself, so Main cannot reach it directly.
+            }
+            #endregion
+
         }
     }
 }
